5ec723ccb8bd5f58e90717d96d9fdedeaa353c1a
Roberto Himmelbauer <robertoh89@gmail.com>
2024-10-24 10:04:01 -0700

    added ModelAdmin Views

--- src/playbracket/admin.py
+++ src/playbracket/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
+from playbracket.models import Sport, League, Player, Event, Match
 
-# Register your models here.
+
+class SportAdmin(admin.ModelAdmin):
+    pass
+
+
+class LeagueAdmin(admin.ModelAdmin):
+    pass
+
+
+class PlayerAdmin(admin.ModelAdmin):
+    pass
+
+
+class EventAdmin(admin.ModelAdmin):
+    pass
+
+
+class MatchAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Sport, SportAdmin)
+admin.site.register(League, LeagueAdmin)
+admin.site.register(Player, PlayerAdmin)
+admin.site.register(Event, EventAdmin)
+admin.site.register(Match, MatchAdmin)
